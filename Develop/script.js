a052f5dd244f86ee694fd03a2b12d43efdc1dc48
Jackie Hodges <jackie.hodges2015@gmail.com>
2021-01-11 12:14:22 -0600

    created upper and lower case arrays

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowerCaseString = "a b c d e f g h i j k l m n o p q r s t u v w x y z";
+var lowerCaseArray = lowerCaseString.split(" ");
+var upperCaseString = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
+var upperCaseArray = upperCaseString.split(" ");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -17,10 +22,12 @@ generateBtn.addEventListener("click", writePassword);
 var passwordLength = prompt("How many characters do you want in your password?");
 if (passwordLength < 8){
   alert("Your password must be at least 8 characters. Try again.");
-}
-else if (passwordLength > 128){
+} else if (passwordLength > 128){
   alert("Your password must be less than 128 characters. Try again.");
+} else {
+  console.log("Password Length: " + passwordLength);
+
+//   create an array with length specified
+
 }
-else {
-  console.log(passwordLength);
-}
+
